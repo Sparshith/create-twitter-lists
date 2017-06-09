3df98b06fd123af56661d3f10ec1764dd0dfa999
Sparshith Sampath <sparshithsampath@gmail.com>
2017-06-09 08:17:35 +0530

    cleanup

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var config = require('./config');
 var Twitter = require('twitter');
 var client = new Twitter(config.auth); 
 
+var handles = "so_radhikal,sayan_sanyal,pujabhattach";
+
 var postParams = {
   slug: 'test',
   owner_screen_name: 'SparshithSampat',
-  screen_name:"so_radhikal,sayan_sanyal,pujabhattach"
+  screen_name: handles
 }
 
 client.post('lists/members/create_all', postParams,  function(error, listDetails, response) {
