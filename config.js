08190e6ba3b7bb56343dcf95c6e512961bd18ded
Sparshith Sampath <sparshithsampath@gmail.com>
2017-06-11 10:18:18 +0530

    Adding empty config file to the repo

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	auth: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+  },
+  userDetails: {
+    listName: '', // This is technically not the name. Insert the last part of the url of the list here.
+    yourHandle: ''
+  }
+}
