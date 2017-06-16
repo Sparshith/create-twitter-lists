3e3b8a81afa506bfef56ac00ad614feb236dffda
Sparshith Sampath <sparshithsampath@gmail.com>
2017-06-16 09:03:45 +0530

    [Fix] Adding gitattributes to retain our version of config on pull

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	auth: {
-		twitter: {
-			consumer_key: '',
-			consumer_secret: '',
-			access_token_key: '',
-			access_token_secret: ''
-		},
-		google: {
+  auth: {
+    twitter: {
+      consumer_key: '',
+      consumer_secret: '',
+      access_token_key: '',
+      access_token_secret: ''
+    },
+    google: {
       key: ''
     }
   },
