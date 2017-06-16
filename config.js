5b76b43db7ec1cdff4a151b317c1a6c39155b406
Sparshith Sampath <sparshithsampath@gmail.com>
2017-06-16 08:46:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 module.exports = {
 	auth: {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+		twitter: {
+			consumer_key: '',
+			consumer_secret: '',
+			access_token_key: '',
+			access_token_secret: ''
+		},
+		google: {
+      key: ''
+    }
   },
   userDetails: {
     listName: '', // This is technically not the name. Insert the last part of the url of the list here.
